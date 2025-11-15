0ca8e3ea7a1058730687971b768a443971f530ab
madhurima1206 <survepallisudhamadhurima@gmail.com>
2025-11-15 12:24:20 +0530

    Update MayorDashboard.js

--- src/components/MayorDashboard.js
+++ src/components/MayorDashboard.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MayorDashboard.css';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
 
 const MayorDashboard = ({ user }) => {
   const [complaints, setComplaints] = useState([]);
